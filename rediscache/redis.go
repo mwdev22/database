9dd8281fa927b0286d01eb1f9a9fcdb40d46fa3a
mwdev22 <maciekwojdyna1@gmail.com>
2025-11-09 18:41:55 +0100

    redis naming

--- rediscache/redis.go
+++ rediscache/redis.go
@@ -8,11 +8,11 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-type Redis struct {
+type RedisCache struct {
 	cli *redis.Client
 }
 
-func NewRedisCache(opt *redis.Options) *Redis {
+func New(opt *redis.Options) *RedisCache {
 	cli := redis.NewClient(opt)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
@@ -21,16 +21,16 @@ func NewRedisCache(opt *redis.Options) *Redis {
 	if err != nil {
 		log.Fatalf("failed to connect to Redis: %v", err)
 	}
-	return &Redis{
+	return &RedisCache{
 		cli: cli,
 	}
 }
 
-func (c *Redis) Get(ctx context.Context, key string) (string, error) {
+func (c *RedisCache) Get(ctx context.Context, key string) (string, error) {
 	return c.cli.Get(ctx, key).Result()
 }
 
-func (c *Redis) GetBytes(ctx context.Context, key string) ([]byte, error) {
+func (c *RedisCache) GetBytes(ctx context.Context, key string) ([]byte, error) {
 	value, err := c.cli.Get(ctx, key).Result()
 	if err != nil {
 		return nil, err
@@ -38,22 +38,22 @@ func (c *Redis) GetBytes(ctx context.Context, key string) ([]byte, error) {
 	return []byte(value), nil
 }
 
-func (c *Redis) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+func (c *RedisCache) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
 	return c.cli.Set(ctx, key, value, expiration).Err()
 }
 
-func (c *Redis) SetBytes(ctx context.Context, key string, value []byte, expiration time.Duration) error {
+func (c *RedisCache) SetBytes(ctx context.Context, key string, value []byte, expiration time.Duration) error {
 	return c.cli.Set(ctx, key, value, expiration).Err()
 }
 
-func (c *Redis) Keys(ctx context.Context, pattern string) ([]string, error) {
+func (c *RedisCache) Keys(ctx context.Context, pattern string) ([]string, error) {
 	return c.cli.Keys(ctx, pattern).Result()
 }
 
-func (c *Redis) Del(ctx context.Context, key string) error {
+func (c *RedisCache) Del(ctx context.Context, key string) error {
 	return c.cli.Del(ctx, key).Err()
 }
 
-func (c *Redis) Exists(ctx context.Context, key string) bool {
+func (c *RedisCache) Exists(ctx context.Context, key string) bool {
 	return c.cli.Exists(ctx, key).Val() > 0
 }
